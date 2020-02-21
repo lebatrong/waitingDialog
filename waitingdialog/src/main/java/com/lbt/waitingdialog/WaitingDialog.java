14d98d3600e70d4f5c86f0ed430377c1c511db6b
batrong2709 <batrong2709@gmail.com>
2020-02-21 09:15:05 +0700

    Add title dialog

--- waitingdialog/src/main/java/com/lbt/waitingdialog/WaitingDialog.java
+++ waitingdialog/src/main/java/com/lbt/waitingdialog/WaitingDialog.java
@@ -1,7 +1,9 @@
 package com.lbt.waitingdialog;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.content.Context;
+import android.widget.TextView;
 
 import com.github.ybq.android.spinkit.SpinKitView;
 
@@ -9,25 +11,69 @@ public class WaitingDialog {
     private static Dialog dialog = null;
 
     public static void showDialog(Context context, int colorID){
-        if(dialog == null){
-            dialog = new Dialog(context);
-            dialog.setContentView(R.layout.waiting_dialog);
 
-            SpinKitView waitingView = dialog.findViewById(R.id.SpinKitLoading);
-            waitingView.setColor(context.getResources().getColor(colorID));
+        if(!((Activity) context).isFinishing()){
+            if(dialog == null){
+                dialog = new Dialog(context);
+                dialog.setContentView(R.layout.waiting_dialog);
 
-            dialog.setCancelable(false);
-            dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+                SpinKitView waitingView = dialog.findViewById(R.id.SpinKitLoading);
+                waitingView.setColor(context.getResources().getColor(colorID));
+
+                dialog.setCancelable(false);
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+            }
+
+            if(!dialog.isShowing())
+                dialog.show();
         }
 
-        if(!dialog.isShowing())
-            dialog.show();
+    }
+
+    public static void showDialog(final Context context){
+        if(!((Activity) context).isFinishing()){
+            if(dialog == null){
+                dialog = new Dialog(context);
+                dialog.setContentView(R.layout.waiting_dialog);
+
+                SpinKitView waitingView = dialog.findViewById(R.id.SpinKitLoading);
+                waitingView.setColor(context.getResources().getColor(R.color.colorPurple));
+                dialog.setCancelable(false);
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+            }
+
+            if(!dialog.isShowing())
+                dialog.show();
+        }
+    }
+
+    public static void showDialog(final Context context, final String title){
+        if(!((Activity) context).isFinishing()){
+            if(dialog == null){
+                dialog = new Dialog(context);
+                dialog.setContentView(R.layout.waiting_dialog_title);
+
+                SpinKitView waitingView = dialog.findViewById(R.id.SpinKitLoading);
+                TextView tvTitle = dialog.findViewById(R.id.tvTitle);
+
+                waitingView.setColor(context.getResources().getColor(R.color.colorPurple));
+                tvTitle.setText(title);
+
+                dialog.setCancelable(false);
+                dialog.getWindow().setBackgroundDrawableResource(android.R.color.transparent);
+            }
+
+            if(!dialog.isShowing())
+                dialog.show();
+        }
     }
 
     public static void dismissDialog(){
-        if(dialog!=null){
-            if(dialog.isShowing())
+        if(dialog != null){
+            if(dialog.isShowing()){
                 dialog.dismiss();
+                dialog = null;
+            }
         }
     }
 }
